bfddc9c36f640014b12d61fce790985bc362fd41
ebubekir <ebushww@gmail.com>
2024-01-22 15:07:32 +0300

    added modal slice

--- app/redux/store/index.ts
+++ app/redux/store/index.ts
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import modalSlice from "../slice/modalSlice";
 
 
 export const store = configureStore({
     reducer: {
-       //
+        modal: modalSlice,
     }
 })
 
